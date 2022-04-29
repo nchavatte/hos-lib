b2b954e0d04ab6e9a146bf0bb702bda9cacc603d
Nicolas CHAVATTE <nchavatte@users.noreply.github.com>
2022-04-29 23:38:30 +0200

    Initializes TDD for serialization

---
+++ NChavatte.HumanOrientedSerialization.Common.Tests/HOS_Serialize.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NChavatte.HumanOrientedSerialization.Common.Tests
+{
+    [TestFixture]
+    public class HOS_Serialize
+    {
+    }
+}
---
+++ NChavatte.HumanOrientedSerialization.Common/HOS.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NChavatte.HumanOrientedSerialization.Common
+{
+    public static class HOS
+    {
+        public static string Serialize(byte[] source)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
