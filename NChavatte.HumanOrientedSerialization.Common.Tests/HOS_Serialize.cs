9e7dfed4d865e1844fa7f81d9dd1f7e820e9b7d2
Nicolas CHAVATTE <nchavatte@users.noreply.github.com>
2022-05-01 10:23:46 +0200

    Adds serialization test case

--- NChavatte.HumanOrientedSerialization.Common.Tests/HOS_Serialize.cs
+++ NChavatte.HumanOrientedSerialization.Common.Tests/HOS_Serialize.cs
@@ -1,6 +1,7 @@
 ﻿using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace NChavatte.HumanOrientedSerialization.Common.Tests
@@ -8,5 +9,30 @@ namespace NChavatte.HumanOrientedSerialization.Common.Tests
     [TestFixture]
     public class HOS_Serialize
     {
+        [TestCase("source.0.bin", "serial-form.0.txt")]
+        public void Should_convert_binary_content_to_serial_form(string sourceFileName, string expectedSerialFormFileName)
+        {
+            // Arrange
+            byte[] source = GetResourceBytes(sourceFileName);
+            string expectedSerialForm = Encoding.ASCII.GetString(GetResourceBytes(expectedSerialFormFileName));
+
+            // Act
+            string actualSerialForm = HOS.Serialize(source);
+
+            // Assert
+            Assert.AreEqual(expectedSerialForm.Trim(), actualSerialForm?.Trim());
+        }
+
+        private static byte[] GetResourceBytes(string resourceFileName)
+        {
+            Type type = typeof(HOS_Serialize);
+            string resourceName = $"{type.Namespace}.Resources.{resourceFileName}";
+            using (Stream stream = type.Assembly.GetManifestResourceStream(resourceName))
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                stream.CopyTo(memoryStream);
+                return memoryStream.ToArray();
+            }
+        }
     }
 }
