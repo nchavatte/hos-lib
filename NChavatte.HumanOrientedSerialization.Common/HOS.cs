29c5ac9caf736517afeee59ca26c051f74b9fdde
Nicolas CHAVATTE <nchavatte@users.noreply.github.com>
2022-05-02 15:42:11 +0200

    :memo: Adds public methods XML documentation

--- NChavatte.HumanOrientedSerialization.Common/HOS.cs
+++ NChavatte.HumanOrientedSerialization.Common/HOS.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 
 namespace NChavatte.HumanOrientedSerialization.Common
 {
@@ -7,6 +6,12 @@ namespace NChavatte.HumanOrientedSerialization.Common
     {
         private const string Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ346789";
 
+        /// <summary>
+        /// Converts bytes into <see href="https://github.com/nchavatte/hos-lib/wiki/Serial-form">serial form</see>
+        /// </summary>
+        /// <param name="source">Bytes to be converted</param>
+        /// <returns>Serial form</returns>
+        /// <exception cref="ArgumentNullException"></exception>
         public static string Serialize(byte[] source)
         {
             if (source == null) throw new ArgumentNullException(nameof(source));
@@ -14,6 +19,15 @@ namespace NChavatte.HumanOrientedSerialization.Common
             return SerialFormBuilder.GetSerialForm(source, Alphabet);
         }
 
+        /// <summary>
+        /// Converts <see href="https://github.com/nchavatte/hos-lib/wiki/Serial-form">serial form</see> into bytes
+        /// or indicates serial form structure error.
+        /// </summary>
+        /// <param name="serialForm"></param>
+        /// <returns>Object that contains deserialized bytes or error cause indication</returns>
+        /// <exception cref="ArgumentException">
+        /// when input serial form is null, empty or white space
+        /// </exception>
         public static DeserializationResult Deserialize(string serialForm)
         {
             if (string.IsNullOrWhiteSpace(serialForm)) throw new ArgumentException("is empty", nameof(serialForm));
