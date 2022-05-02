b7ca7a487da6ca4199210956ba0d27bee2f57f9f
Nicolas CHAVATTE <nchavatte@users.noreply.github.com>
2022-05-02 15:17:58 +0200

    Adds serialization test cases

--- NChavatte.HumanOrientedSerialization.Common.Tests/HOS_Serialize.cs
+++ NChavatte.HumanOrientedSerialization.Common.Tests/HOS_Serialize.cs
@@ -1,7 +1,5 @@
-﻿using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using NChavatte.HumanOrientedSerialization.Common.Tests.Resources;
+using NUnit.Framework;
 using System.Text;
 
 namespace NChavatte.HumanOrientedSerialization.Common.Tests
@@ -10,29 +8,19 @@ namespace NChavatte.HumanOrientedSerialization.Common.Tests
     public class HOS_Serialize
     {
         [TestCase("source.0.bin", "serial-form.0.txt")]
+        [TestCase("source.1.bin", "serial-form.1.txt")]
+        [TestCase("source.2.bin", "serial-form.2.txt")]
         public void Should_convert_binary_content_to_serial_form(string sourceFileName, string expectedSerialFormFileName)
         {
             // Arrange
-            byte[] source = GetResourceBytes(sourceFileName);
-            string expectedSerialForm = Encoding.ASCII.GetString(GetResourceBytes(expectedSerialFormFileName));
+            byte[] source = ResourceProvider.GetResourceBytes(sourceFileName);
+            string expectedSerialForm = Encoding.ASCII.GetString(ResourceProvider.GetResourceBytes(expectedSerialFormFileName));
 
             // Act
             string actualSerialForm = HOS.Serialize(source);
 
             // Assert
             Assert.AreEqual(expectedSerialForm.Trim(), actualSerialForm?.Trim());
-        }
-
-        private static byte[] GetResourceBytes(string resourceFileName)
-        {
-            Type type = typeof(HOS_Serialize);
-            string resourceName = $"{type.Namespace}.Resources.{resourceFileName}";
-            using (Stream stream = type.Assembly.GetManifestResourceStream(resourceName))
-            using (MemoryStream memoryStream = new MemoryStream())
-            {
-                stream.CopyTo(memoryStream);
-                return memoryStream.ToArray();
-            }
         }
     }
 }
---
+++ NChavatte.HumanOrientedSerialization.Common.Tests/Resources/ResourceProvider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace NChavatte.HumanOrientedSerialization.Common.Tests.Resources
+{
+    internal static class ResourceProvider
+    {
+        internal static byte[] GetResourceBytes(string resourceFileName)
+        {
+            Type type = typeof(ResourceProvider);
+            string resourceName = $"{type.Namespace}.{resourceFileName}";
+            using (Stream stream = type.Assembly.GetManifestResourceStream(resourceName))
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                stream.CopyTo(memoryStream);
+                return memoryStream.ToArray();
+            }
+        }
+    }
+}
